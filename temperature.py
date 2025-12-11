a25427ee7bd336d60cb3b18cd817219b290862ac
laxman2000-glitch <abbireddylaxman@gmail.com>
2025-12-11 18:29:55 +0530

    Implement temperature conversion functionality
    Add temperature conversion from Celsius to Kelvin and Fahrenheit.

---
+++ temperature.py
@@ -0,0 +1,9 @@
+a= int(input("enter temperature:"))
+b= input("enter units")
+k= a+ 273.15
+k2= (a * 9/5) + 32 
+if a+ 273.15 and (a * 9/5) + 32 :
+    print(f"temperature in kelvin is {k} ") 
+    print(f"temperature in fahrenheit is {k} ") 
+else: 
+    print("give correct units")
